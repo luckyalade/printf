cdfc4aa294adcf7d11f668f04a6043be411a7a91
solomonuche <solomonuche@gmail.com>
2023-07-23 13:08:18 -0700

    added neccessary files for this project, header file, putchar and printf file

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - a function that prints an integer, char, or strings based
+ * based on different specifier.
+ * @format: format specifier
+ *
+ * Return: (0)
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN-H
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
