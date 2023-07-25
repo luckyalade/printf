7273d3e7cde018144a3b7a158a49d8f5e931b454
luckyalade <luckyalade309@gmail.com>
2023-07-25 08:15:43 -0700

    updated print size function

--- printSize.c
+++ printSize.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_size -Calculates the size to cast the argument
+ * printSize - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
