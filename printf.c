83f516c31f84123b267a66bfb9df41bf8274bc60
solomonuche <solomonuche@gmail.com>
2023-07-24 20:20:35 -0700

    team project

--- printf.c
+++ printf.c
@@ -16,53 +16,43 @@ int _printf(const char *format, ...)
 		{"c", printchar}, {"s", printstring}, {"d", printf_int},
 		{"i", printf_dec}, {NULL, NULL}};
 	int i = 0, j, count = 0;
-	int unknownSpecifier;
 	va_list args;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
-	while (format[i] != '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-			{
-				_putchar(format[i]);
-				count++;
-			}
-			else if (format[i] != '\0')
-			{
-				unknownSpecifier = 1;
-				for (j = 0; ops[j].s != NULL; j++)
-				{
-					if (format[i] == *(ops[j].s))
-					{
-						unknownSpecifier = 0;
-						ops[j].f(args);
-						count++;
-						break;
-					}
-				}
-				if (unknownSpecifier)
-				{
-					_putchar('%');
-					count++;
-					_putchar(format[i]);
-					count++;
-				}
-			}
-		}
-		else if (format[i] != '\0')
-		{
-			_putchar(format[i]);
+		if (format[i] != '%')
 			count++;
+			_putchar(format[i]);
+		else if (format[i + 1] == '%')
+		{
+			count++;
+			_putchar('%');
+			i++;
 		}
-		i++;
+		else if (format[i + 1] != 'c' && format[i + 1] != 's'
+				&& format[i + 1] != 'i' && format[i + 1] != 'd'
+				&& format[i + 1] != 'R' && format[i + 1] != 'r')
+		{
+			counter++;
+			_putchar('%');
+			counter++;
+			_putchar(format[i + 1]);
+			i++;
+		}
+		else
+		{
+			for (j = 0; ops[j],s != NULL; j++)
+				if (format[i + 1] == *(ops[j].s)
+					counter++;
+					ops[j].f(args);
+			i++;
+		}
 	}
-
-	va_end(args);
-	return (count);
+	va_end(ap);
+	return (counter);
 }
