909289a88ae6f6f260c89b1dc1e672b03efbf332
solomonuche <solomonuche@gmail.com>
2023-07-25 01:31:08 -0700

    Team project

--- printf.c
+++ printf.c
@@ -33,6 +33,10 @@ int _printf(const char *format, ...)
 				_putchar(format[i]);
 				count++;
 			}
+			else if (format[i] =='\0')
+			{
+				return (-1);
+			}
 			else if (format[i] != '\0')
 			{
 				unknownSpecifier = 1;
