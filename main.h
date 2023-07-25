7121febf7dd9d131dc6b901b318d4e9f53c578bc
solomonuche <solomonuche@gmail.com>
2023-07-25 05:48:59 -0700

    updated header file

--- main.h
+++ main.h
@@ -75,7 +75,7 @@ int print_pointer(va_list types, char buffer[],
 int print_flags(const char *format, int *i);
 int print_width(const char *format, int *i, va_list list);
 int print_precision(const char *format, int *i, va_list list);
-int print_size(const char *format, int *i);
+int printSize(const char *format, int *i);
 
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
